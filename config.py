44d171157b4e309b94e42f40c47f87d0f4fcd5b4
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-07-04 13:43:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5761434331:AAHLjGg_Hb_ZyXUa-9YKrT_EjvJd_D2XX80")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6074522431:AAEsif1gAvlgfM0zYz9zKdJeD7flp8mJX2Y")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "16743442"))
@@ -20,7 +20,12 @@ APP_ID = int(os.environ.get("APP_ID", "16743442"))
 API_HASH = os.environ.get("API_HASH", "12bbd720f4097ba7713c5e40a11dfd2a")
 
 # ID Channel Database
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001750485720"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001912633959"))
+
+#📢 -1001912633959
+#🏷 FSB Database
+#🕑 Message sent at 04/07/23 10:07:40
+
 
 # NAMA OWNER
 OWNER = os.environ.get("OWNER", "The_CopyNinjaKakashi")
@@ -39,8 +44,8 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "postgres://qjhpzlli:rzj7UP6Qe_uhHrNxtwqemrHZuh7euWf-@trumpet.db.elephantsql.com/qjhpzlli")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001928279167"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001809308823"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001209619698"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
@@ -68,7 +73,7 @@ DISABLE_CHANNEL_BUTTON = strtobool(os.environ.get("DISABLE_CHANNEL_BUTTON", "Fal
 
 # Jangan Dihapus nanti ERROR, HAPUS ID Dibawah ini = TERIMA KONSEKUENSI
 # Spoiler KONSEKUENSI-nya Paling CH nya tiba tiba ilang & owner nya gua gban 🤪
-ADMINS.extend((844432220, 6198858059, 778393824, 6299128233))
+ADMINS.extend((5254177746, 6198858059, 5048278767, 6299128233))
 
 
 LOG_FILE_NAME = "logs.txt"
