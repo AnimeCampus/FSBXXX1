5897982eb142c066d3589ea707d5249a26ae6511
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-07-16 21:24:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6022510818:AAGutnwn7eW5tF3yXRF67QG--6ohEAbI1RI")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6053257808:AAF_XqeptZAh6Wfhyg3qmywXO5c1sHTwm6g")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "16743442"))
@@ -44,8 +44,8 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "postgres://qmxstnyo:Bi8dFQVcN1maP_rGFj_OFrMVLwrB1inH@tyke.db.elephantsql.com/qmxstnyo")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001860678095"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001209619698"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001209619698"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
